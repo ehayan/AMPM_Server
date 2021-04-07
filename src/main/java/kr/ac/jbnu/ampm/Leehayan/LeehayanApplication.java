d0da91a295099bc8cdc511888264dc7a2c8f663e
LeeHayan <hayan8029@gmail.com>
2021-04-07 15:36:53 +0900

    hw 1

---
+++ src/main/java/kr/ac/jbnu/ampm/Leehayan/LeehayanApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.jbnu.ampm.Leehayan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeehayanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeehayanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/jbnu/ampm/Leehayan/LeehayanApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.jbnu.ampm.Leehayan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeehayanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
