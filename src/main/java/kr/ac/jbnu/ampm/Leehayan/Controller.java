894bfe6587a4b6f17b3ae1da558a84c5e0155ae3
LeeHayan <hayan8029@gmail.com>
2021-05-13 21:48:29 +0900

    put

--- src/main/java/kr/ac/jbnu/ampm/Leehayan/Controller.java
+++ src/main/java/kr/ac/jbnu/ampm/Leehayan/Controller.java
@@ -15,6 +15,8 @@ import java.util.Map;
 public class Controller {
     //get부터 id로 받는거 -> type으로 받기 post, delete
     //1. testdb 2. get all 3. get id 4. post 5. delete
+
+    //DB
     private static HashMap<String, ArrayList<Map<String, Object>>> testDBHashMap = new HashMap<String, ArrayList<Map<String, Object>>>();
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
@@ -32,7 +34,7 @@ public class Controller {
         return responseEntity;
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)//id받았을 때, id값만 출력
     @ResponseBody
     public ResponseEntity<?> getResponseEntity(HttpServletRequest request, @PathVariable String id) {
         ResponseEntity<?> responseEntity = null;
@@ -56,19 +58,18 @@ public class Controller {
     @ResponseBody
     public ResponseEntity<?> postResponseEntity(HttpServletRequest request, @PathVariable String id, @RequestBody Map<String, Object> requestMap) {
         ResponseEntity<?> responseEntity = null;
-        ArrayList<Map<String, Object>> postValueArrayList = null;
+        ArrayList<Map<String, Object>> postValueArrayList = null;//data 임시저장, replace에 사용
 
         if (id != null && !id.equals("")) {
-
-            if (testDBHashMap.containsKey(id)) {
-                postValueArrayList = testDBHashMap.get(id);
+            if (testDBHashMap.containsKey(id)) {//check DB has key
+                postValueArrayList = testDBHashMap.get(id);//있으면 db값 가져옴
             } else {
-                postValueArrayList = new ArrayList<Map<String, Object>>();
+                postValueArrayList = new ArrayList<Map<String, Object>>();//없으면 새로만듦
             }
 
             postValueArrayList.add(requestMap);
 
-            if (testDBHashMap.containsKey(id)) {
+            if (testDBHashMap.containsKey(id)) {//있을경우 replace인데 안됨, 계속 추가함(Arraylist라서 순차적으로 들어감)
                 testDBHashMap.replace(id, postValueArrayList);
             } else {
                 testDBHashMap.put(id, postValueArrayList);
@@ -99,6 +100,36 @@ public class Controller {
             }
 
         } else {
+            responseEntity = new ResponseEntity<>("NOT_CONTAIN", HttpStatus.BAD_REQUEST);
+        }
+
+        return responseEntity;
+    }
+
+    @RequestMapping(value = "/put/{id}", method = RequestMethod.PUT, produces = "application/json")
+    @ResponseBody
+    public ResponseEntity<?> putResponseEntity(HttpServletRequest request, @PathVariable String id, @RequestBody Map<String,Object> requestMap){
+        ResponseEntity<?> responseEntity = null;
+        ArrayList<Map<String,Object>> postValueArrayList = null;
+
+        if(!testDBHashMap.isEmpty()){
+            if(id!=null && !id.equals("")&&testDBHashMap.containsKey(id)){
+            postValueArrayList = new ArrayList<Map<String,Object>>();//새로운 db
+            postValueArrayList.add(requestMap);
+
+            if(postValueArrayList.get(0).keySet().equals(testDBHashMap.get(id).get(0).keySet())){//keySet = hashMap에 저장된 모든 key
+                testDBHashMap.replace(id, postValueArrayList);
+                responseEntity = new ResponseEntity<>(requestMap, HttpStatus.OK);
+
+            }else{
+                responseEntity = new ResponseEntity<>("NOT_CONTAIN", HttpStatus.NOT_FOUND);
+            }
+
+            }else{
+                responseEntity = new ResponseEntity<>("NOT_CONTAIN", HttpStatus.NOT_FOUND);
+            }
+
+        }else {
             responseEntity = new ResponseEntity<>("NOT_CONTAIN", HttpStatus.BAD_REQUEST);
         }
 
